7097ec12c146aea18578e8da59f56afb54504416
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2024-11-05 17:53:17 +0100

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     const contacts = await Contact.find();
     res.status(200).json({
       status: 200,
-      message: 'Successfully found contacts!',
+      message: 'Successfully found contacts!!',
       data: contacts,
     });
   } catch (error) {
