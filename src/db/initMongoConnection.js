d5f5c0245385f73bbabf50f337ceb028100237a6
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2024-11-06 12:32:20 +0100

    fixing remarks

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
-import { env } from '../utils/env.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 async function initMongoConnection() {
   try {
-    const user = env('MONGODB_USER');
-    const pwd = env('MONGODB_PASSWORD');
-    const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
+   
+    const user = process.env.MONGODB_USER;
+    const pwd = process.env.MONGODB_PASSWORD;
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
     const DB_URL = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=mongodb`;
+    
     await mongoose.connect(DB_URL);
     console.log('Database connection successfully');
   } catch (error) {
@@ -15,4 +20,5 @@ async function initMongoConnection() {
     throw error;
   }
 }
+
 export { initMongoConnection };
