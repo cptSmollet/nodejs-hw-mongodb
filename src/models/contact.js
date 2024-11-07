673f674c01df7522aee3641c63b3d3605078f9a9
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2024-11-07 14:28:56 +0100

    Update contact.js

--- src/models/contact.js
+++ src/models/contact.js
@@ -8,14 +8,24 @@ const contactSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: [true, 'Email is required'],
-      unique: true, 
+      required: [false, 'Email is required'], 
+      unique: true,
       match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], 
     },
     phone: {
       type: String,
       required: [true, 'Phone is required'],
       unique: true, 
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,  
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],  
+      required: [true, 'Contact type is required'],
+      default: 'personal',  
     },
   },
   { timestamps: true }
