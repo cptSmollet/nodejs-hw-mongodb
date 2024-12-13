0abbd1d115cb7c24a13efde77bc69acd9e652a2d
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2024-12-13 12:39:20 +0100

    raw build

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -23,6 +24,8 @@ function setupServer() {
   );
 
   app.use(express.json());
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(cookieParser());
 
